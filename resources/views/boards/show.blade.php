a2bcfd7638e2ed4b7b9957f11ad946d479d7963a
sato akihide <akihide315@gmail.com>
2019-11-02 23:47:39 +0900

    Add: show画面に投稿者名表示

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -5,14 +5,18 @@
   <div class="item-wrapper">
     <div class="item-header">
       <span class="date">{{$board->created_at}}</span>
-      <div class="btn-group">
-        <a class="btn btn-primary btn-sm" href="{{ route('boards.edit', ['board'=>$board]) }}">修正する</a>
-        <form method="post" action="{{ route('boards.destroy', ['board'=>$board]) }}">
-          @csrf
-          @method('DELETE')
-          <input type="submit" value="削除" class="btn btn-danger btn-sm" onclick='return confirm("本当に削除しますか？");'>
-        </form>
-      </div>
+      @if ($board->user_id == Auth::id())
+        <div class="btn-group">
+          <a class="btn btn-primary btn-sm" href="{{ route('boards.edit', ['board'=>$board]) }}">修正する</a>
+          <form method="post" action="{{ route('boards.destroy', ['board'=>$board]) }}">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除" class="btn btn-danger btn-sm" onclick='return confirm("本当に削除しますか？");'>
+          </form>
+        </div>
+      @else
+        <span class="user-name">投稿者: {{$board->user->name}}</span>
+      @endif
     </div>
     <div class="item-title">{{$board->title}}</div>
     <div class="item-body">{{$board->body}}</div>
