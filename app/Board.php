d74d90c1523eabdef514a1a3672455673392575c
sato akihide <akihide315@gmail.com>
2019-11-02 19:35:26 +0900

    Add: Boardモデル作成

---
+++ app/Board.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+  public function user()
+    {
+      return $this->belongsTo('App\User');
+    }
+    
+  protected $fillable = ['user_id', 'title', 'body'];
+}
