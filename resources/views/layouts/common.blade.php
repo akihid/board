8ef452d9fe8dc64cf939c477272172333b6ef278
sato akihide <akihide315@gmail.com>
2019-11-02 19:00:53 +0900

    Add: navbar作成

---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Board</title>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        @include('layouts.navbar')
+        @yield('content')
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light pl-5 pr-5 pt-2 pb-2">
+  <a class="navbar-brand text-white" href="/">Board</a>
+  <div class="collapse navbar-collapse" id="Navbar">
+    <ul class="navbar-nav ml-auto mr-5">
+      @guest
+        <!-- 非ログイン時 -->
+        <li class="nav-item">
+          <a class="nav-link text-white" href="{{ route('login') }}">ログイン</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="{{ route('register') }}">登録</a>
+        </li>
+      @else
+        <!-- ログイン時 -->
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">お気に入り一覧</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" id="post-link" href="#">投稿する</a>
+        </li>
+        <!-- ドロップダウンメニュー -->
+        <li class="nav-item dropdown">
+          <a class="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              ログアウト
+            </a>
+          </div>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
+        </li>
+      @endguest
+    </ul>
+  </div>
+</nav>
