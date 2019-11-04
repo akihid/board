b089add35c74c0b557f49d44167cd9ab56bbf0c1
sato akihide <akihide315@gmail.com>
2019-11-04 21:18:51 +0900

    Add: 投稿一覧にユーザーアイコンを表示

--- resources/views/boards/_index.blade.php
+++ resources/views/boards/_index.blade.php
@@ -1,13 +1,23 @@
 @foreach ($boards as $board)
-  <a href="{{ route('boards.show', ['board'=>$board]) }}">
-    <div class="board-box">
-      <div class="board-box-left"></div>
-      <div class="board-box-right">
+  
+  <div class="board-box">
+    <div class="board-box-left">
+      <a href="{{ route('users.show', ['user'=>$board->user]) }}">
+        @isset ($board->user->avator_url)
+          <img class="avator_img_min" src="{{ asset('/storage/avator_images/'.$board->user->avator_url) }}" alt="アバター画像">
+        @else
+          <img class="avator_img_min" src="/images/default.jpeg" alt="アバター画像">
+        @endisset
+      </a>
+    </div>
+    <div class="board-box-right">
+      <a href="{{ route('boards.show', ['board'=>$board]) }}">
         <span class="board-title">{{ $board->title }}</span>
         <div class="board-details">
           <div class="board-date">投稿日時：{{ $board->created_at }}</div>
         </div>
-      </div>
+      </a>
     </div>
-  </a>
+  </div>
+  
 @endforeach
