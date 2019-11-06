83a1349f5581867b8021e8ad0927a6865688c5c0
sato akihide <akihide315@gmail.com>
2019-11-06 20:30:52 +0900

    Fix: seedデータ作成順序変更

--- database/seeds/BoardsTableSeeder.php
+++ database/seeds/BoardsTableSeeder.php
@@ -59,23 +59,23 @@ class BoardsTableSeeder extends Seeder
           ***
         EOF
       );
+      //ページネーション確認用データ
+      $faker = Faker::create('ja_JP');
+      for ($i = 1; $i <= 10; $i++) {
+        $user = App\User::inRandomOrder()->first();
+        App\Board::create([
+          'title' => $faker->realText(10),
+          'body' => $faker->realText(20),
+          'user_id' => $user->id,
+        ]);
+      }
+
       // マークダウンデータ
       foreach ($data as $key => $value) {
         $user = App\User::inRandomOrder()->first();
         App\Board::create([
           'title' => $key,
           'body' => $value,
-          'user_id' => $user->id,
-        ]);
-      }
-
-      //ページネーション確認用データ
-      $faker = Faker::create('ja_JP');
-      for ($i = 1; $i <= 10; $i++) {
-        $user = App\User::inRandomOrder()->first();
-        App\Board::create([
-          'title' => $faker->word,
-          'body' => $faker->realText(20),
           'user_id' => $user->id,
         ]);
       }
