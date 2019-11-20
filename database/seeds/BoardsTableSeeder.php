249e1d077d93d7b450ef8cb51ab14ee403c64c77
sato akihide <akihide315@gmail.com>
2019-11-20 18:46:00 +0900

    fix: seederの最終行削除

--- database/seeds/BoardsTableSeeder.php
+++ database/seeds/BoardsTableSeeder.php
@@ -5,79 +5,79 @@ use Faker\Factory as Faker;
 
 class BoardsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-      DB::table('boards')->delete();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('boards')->delete();
 
-      $data = array(
-        '見出しのサンプル' => <<< EOF
-          # 見出し1
-          ## 見出し2
-          ### 見出し3
-          #### 見出し4
-          ##### 見出し5
-        EOF
-        ,
-        '箇条書きリスト' => <<< EOF
-          - リスト1
-            - ネスト リスト1_1
-              - ネスト リスト1_1_1
-              - ネスト リスト1_1_2
-            - ネスト リスト1_2
-          - リスト2
-          - リスト3
-        EOF
-        ,
-        'シンタックスハイライト' => <<< EOF
-          ```ruby
-          　class Hoge
-          　  def hoge
-          　    print 'hoge'
-          　  end
-          　end
-          ```
-        EOF
-        ,
-        'インライン表示' => <<< EOF
-          `インライン表示`
-          ふつうのテキスト
-        EOF
-        ,
-        'リンクの表示' => <<< EOF
-          [Qiita](http://qiita.com/)
-        EOF
-        ,
-        '太字とか斜体とか' => <<< EOF
-          **テキスト**
-          *テキスト*
-          ~~テキスト~~
-          ***
-        EOF
-      );
-      //ページネーション確認用データ
-      $faker = Faker::create('ja_JP');
-      for ($i = 1; $i <= 10; $i++) {
-        $user = App\User::inRandomOrder()->first();
-        App\Board::create([
-          'title' => $faker->realText(10),
-          'body' => $faker->realText(20),
-          'user_id' => $user->id,
-        ]);
-      }
-
-      // マークダウンデータ
-      foreach ($data as $key => $value) {
-        $user = App\User::inRandomOrder()->first();
-        App\Board::create([
-          'title' => $key,
-          'body' => $value,
-          'user_id' => $user->id,
-        ]);
-      }
+    $data = array(
+      '見出しのサンプル' => <<< EOF
+        # 見出し1
+        ## 見出し2
+        ### 見出し3
+        #### 見出し4
+        ##### 見出し5
+      EOF
+      ,
+      '箇条書きリスト' => <<< EOF
+        - リスト1
+          - ネスト リスト1_1
+            - ネスト リスト1_1_1
+            - ネスト リスト1_1_2
+          - ネスト リスト1_2
+        - リスト2
+        - リスト3
+      EOF
+      ,
+      'シンタックスハイライト' => <<< EOF
+        ```ruby
+        　class Hoge
+        　  def hoge
+        　    print 'hoge'
+        　  end
+        　end
+        ```
+      EOF
+      ,
+      'インライン表示' => <<< EOF
+        `インライン表示`
+        ふつうのテキスト
+      EOF
+      ,
+      'リンクの表示' => <<< EOF
+        [Qiita](http://qiita.com/)
+      EOF
+      ,
+      '太字とか斜体とか' => <<< EOF
+        **テキスト**
+        *テキスト*
+        ~~テキスト~~
+        ***
+      EOF
+    );
+    //ページネーション確認用データ
+    $faker = Faker::create('ja_JP');
+    for ($i = 1; $i <= 10; $i++) {
+      $user = App\User::inRandomOrder()->first();
+      App\Board::create([
+        'title' => $faker->realText(10),
+        'body' => $faker->realText(20),
+        'user_id' => $user->id,
+      ]);
     }
+
+    // マークダウンデータ
+    foreach ($data as $key => $value) {
+      $user = App\User::inRandomOrder()->first();
+      App\Board::create([
+        'title' => $key,
+        'body' => $value,
+        'user_id' => $user->id,
+      ]);
+    }
+  }
 }
--- database/seeds/LikesTableSeeder.php
+++ database/seeds/LikesTableSeeder.php
@@ -4,20 +4,20 @@ use Illuminate\Database\Seeder;
 
 class LikesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-      $boards = App\Board::latest('created_at')->get();
-      foreach ($boards as $board) {
-        $user = App\User::inRandomOrder()->first();
-        App\Like::create([
-          'user_id' => $user->id,
-          'board_id' => $board->id,
-        ]);
-      }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $boards = App\Board::latest('created_at')->get();
+    foreach ($boards as $board) {
+      $user = App\User::inRandomOrder()->first();
+      App\Like::create([
+        'user_id' => $user->id,
+        'board_id' => $board->id,
+      ]);
     }
+  }
 }
