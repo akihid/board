f28255db5e31883325bfe20f304709e3edfd62d4
sato akihide <akihide315@gmail.com>
2019-11-22 13:09:54 +0900

    Add: 投稿画面レスポンシブ対応

--- resources/views/boards/create.blade.php
+++ resources/views/boards/create.blade.php
@@ -5,17 +5,21 @@
 
 <form class="board-page-wrapper" action="{{ route('boards.store') }}" method="POST">
 @csrf
-  <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
+  <div class="form-group row">
+    <input type="text" class="form-control col-md-10 m-1" id="title-input" placeholder="タイトル" name="title">
+  </div>
   <div class="row">
-    <div class="col-6 m-1">
+    <div class="markdown_field col-md-5 m-1">
       <textarea name="body" id="markdown_editor_textarea" cols="30" rows="10" class="form-control"></textarea>
     </div>
-    <div class="col-6 m-1">
+    <div class="markdown_field col-md-5 m-1">
       <div id="markdown_preview"></div>
     </div>
   </div>
-  <div class="board-page-footer">
-    <input type="submit" class="post-button m-1" value="投稿">
+  <div class="row">
+    <div class="board-page-footer col-md-10">
+      <input type="submit" class="post-button m-1" value="投稿">
+    </div>
   </div>
 </form>
 
--- resources/views/boards/edit.blade.php
+++ resources/views/boards/edit.blade.php
@@ -6,21 +6,23 @@
 <form class="board-page-wrapper" action="{{ route('boards.update', ['board' => $board]) }}" method="POST">
   @csrf
   @method('PATCH')
-  <div class="form-group">
-    <input type="text" class="form-control m-1" id="title-input" name="title" value="{{ old('title', $board->title) }}">
+  <div class="form-group row">
+    <input type="text" class="form-control col-md-10 m-1" id="title-input" name="title" value="{{ old('title', $board->title) }}">
   </div>
   <div class="row">
-    <div class="col-6 m-1">
+    <div class="markdown_field col-md-5 m-1">
       <div class="form-group">
         <textarea name="body" id="markdown_editor_textarea" cols="30" rows="10" class="form-control">{{ old('body', $board->body) }}</textarea>
       </div>
     </div>
-    <div class="col-6 m-1">
+    <div class="markdown_field col-md-5 m-1">
       <div id="markdown_preview"></div>
     </div>
   </div>
-  <div class="board-page-footer">
-    <input type="submit" class="post-button m-1" value="修正">
+  <div class="row">
+    <div class="board-page-footer col-md-10">
+      <input type="submit" class="post-button m-1" value="修正">
+    </div>
   </div>
 </form>
 
