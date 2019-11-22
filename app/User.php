8790d845ce7d3d67d84f6f2a2ea9c9df38f3ad6a
sato akihide <akihide315@gmail.com>
2019-11-22 21:02:21 +0900

    Add: twitterログイン機能実装

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avator_url', 'detail'
+        'name', 'email', 'password', 'avator_url', 'detail', 'auth_id'
     ];
 
     /**
