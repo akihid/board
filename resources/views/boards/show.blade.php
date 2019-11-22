28cd12334b547d92618600e2717457f489fc2a20
sato akihide <akihide315@gmail.com>
2019-11-22 17:15:10 +0900

    Add: board詳細画面レスポンシブ対応

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -2,84 +2,40 @@
 @section('content')
 
 <div class="item-page-wrapper">
-  <div class="item-wrapper">
-    <div class="item-header d-flex align-items-center">
-      <span class="date">{{$board->created_at}}</span>
-      <span>投稿者: <a class="text-primary" href="{{ route('users.show', ['user'=>$board->user]) }}">{{$board->user->name}}</a></span>
-      @if ($board->user_id == Auth::id())
-        <div class="btn-group">
-          <a class="btn btn-primary btn-sm" href="{{ route('boards.edit', ['board'=>$board]) }}">修正する</a>
-          <form method="post" action="{{ route('boards.destroy', ['board'=>$board]) }}">
-            @csrf
-            @method('DELETE')
-            <input type="submit" value="削除" class="btn btn-danger btn-sm" onclick='return confirm("本当に削除しますか？");'>
-          </form>
+  @auth
+  <div class="container ml-4 pl-2">
+    <div class="row">
+      @include('boards/_like')
+      <span class="btn btn-sm bg-secondary text-white small"><i class="fas fa-comment fa-fw"></i>コメント件数{{ $board->comments->count()}}
+    </div>
+  </div>
+  @endauth
+  <div class="item-wrapper m-4 p-2">
+    <div class="item-header d-flex align-items-center container m-0 p-0">
+      <div class="row w-100">
+        <div class="date col-md-3 col-7 small">
+          {{$board->created_at->format('Y年m月d日')}}
+          <a class="text-primary" href="{{ route('users.show', ['user'=>$board->user]) }}">{{$board->user->name}}</a>
         </div>
-      @endif
 
-      @auth
-      @if ($like)
-          <form method="post" action="{{ route('likes.destroy', ['like'=>$like]) }}">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger btn-sm ml-3"><i class="fas fa-thumbs-up fa-fw"></i>いいね済み！{{ $board->likes->count()}}</button>
-          </form>
-        @else
-          <form method="POST" action="{{ route('likes.store') }}">
-            @csrf
-
-            <input name="board_id" type="hidden" value="{{ $board->id }}">
-            <input name="user_id" type="hidden"  value="{{ Auth::id() }}">
-            <button type="submit" class="btn btn-primary btn-sm ml-3"><i class="fas fa-thumbs-up fa-fw"></i>いいね！{{ $board->likes->count()}}</button>
-          </form>
-        @endif
-      @endauth
+          @if ($board->user_id == Auth::id())
+            <div class="btn-group col-md-2 col-5">
+              <a class="btn btn-primary btn-sm" href="{{ route('boards.edit', ['board'=>$board]) }}">修正する</a>
+              <form method="post" action="{{ route('boards.destroy', ['board'=>$board]) }}">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="削除" class="btn btn-danger btn-sm" onclick='return confirm("本当に削除しますか？");'>
+              </form>
+            </div>
+          @endif
+      </div>
+      
     </div>
     <div class="item-title">{{$board->title}}</div>
     <div class="item-body">{{$board->body}}</div>
   </div>
 
-  <div class="comment-wrapper">
-    <h5 class="mb-4">コメント</h5>
-    @include('errors.form_errors')
-
-    @forelse($board->comments as $comment)
-      <div class="comment-main border-top">
-        <div class="comment-body">{!!  nl2br($comment->body) !!}</div>
-        <div class="comment-footer">
-          <span>投稿者: <a class="text-primary" href="{{ route('users.show', ['user'=>$comment->user]) }}">{{$comment->user->name}}</a></span>
-          <span class="date">{{$comment->created_at}}</span>
-          @if ($comment->user_id == Auth::id())
-            <form method="post" action="{{ route('comments.destroy', ['comment'=>$comment]) }}">
-              @csrf
-              @method('DELETE')
-              <input type="submit" value="削除" class="btn btn-danger btn-sm" onclick='return confirm("本当に削除しますか？");'>
-            </form>
-          @endif
-        </div>
-      </div>
-    @empty
-      <p>コメントはまだありません。</p>
-    @endforelse
-
-    @auth
-      <form class="mb-4" method="POST" action="{{ route('comments.store') }}">
-        @csrf
-
-        <input name="board_id" type="hidden" value="{{ $board->id }}">
-        <input name="user_id" type="hidden"  value="{{ Auth::id() }}">
-
-        <div class="form-group">
-          <label for="body">本文</label>
-          <textarea id="body" name="body" class="form-control" rows="4">{{ old('body') }}</textarea>
-        </div>
-
-        <div class="mt-4 text-center">
-          <button type="submit" class="btn btn-primary">コメントする</button>
-        </div>
-      </form>
-    @endauth
-  </div>
+  @include('boards/_comment')
 </div>
 
 @endsection
